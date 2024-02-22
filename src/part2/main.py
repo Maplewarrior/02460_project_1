1b322bd0245d969e196b58d83a6e11a447783da7
Niels Raunkjær Holm <nielsraunkjaer@gmail.com>
2024-02-22 10:59:41 +0100

    Refactor part2-sample methods to save to 4 individual pdfs

--- src/part2/main.py
+++ src/part2/main.py
@@ -136,12 +136,9 @@ if __name__ == "__main__":
         with torch.no_grad():
             n_samples = 4
             samples = (model.sample((n_samples,D))).cpu()
+            # Transform the samples back to the original space
+            samples = samples / 2 + 0.5
             for i in range(n_samples):
-                
-                # Transform the samples back to the original space
-                samples = samples / 2 + 0.5
-                
-                save_path =  f"{(args.samples).split('.png')[0]}_{i}.pdf" # make save_path in format {save_loc}/{filename}_{1,2,...,n_samples}.png
-
-                save_image(samples[i].view(1, 1, 28, 28), save_path)
+                save_path =  f"{(args.samples).split('.pdf')[0]}_{i}.pdf" # make save_path in format {save_loc}/{filename}_{1,2,...,n_samples}.{ext}
+                save_image(samples[i].view(1, 1, 28, 28), save_path, format='pdf')
     
